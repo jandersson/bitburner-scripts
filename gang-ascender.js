0301b327acad0662a237ecf36393936e5bf4f99e
Jonas Andersson <jonas@andersson.io>
2022-02-15 19:43:27 +0100

    Create script to automate gang member ascension

---
+++ gang-ascender.js
@@ -0,0 +1,21 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    while (true){
+        var members = ns.gang.getMemberNames();
+        for (let i in members){
+            var member = members[i];
+            var result = ns.gang.getAscensionResult(member);
+            for (let j in result){
+                if (j == "respect"){
+                    continue;
+                }
+                if (result[j] >= 2){
+                    ns.gang.ascendMember(member);
+                    ns.tprint("Ascended member: ", member);
+                    break;
+                }
+            }
+        }
+        await ns.sleep(60_000);
+    }
+}
